269a9e60f3c0c068d2c74230224fd440aee1d970
ngocyolo12345 <tranguyenngoc2862002@gmail.com>
2023-12-05 15:23:04 +0700

    adding vuetify

---
+++ src/rounter/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomePage from '../components/HomePage';
+
+// Vue.useAttrs(VueRouter);
+// const routers = [
+//     {
+//         name: 'Home',
+//         path: '/',
+//         component: HomePage
+//     }
+// ];
+
+// const router = new VueRouter({
+//     mode:'history',
+//     base:'localhosr:8000',
+//     routers
+// });
+
+// export default router;
