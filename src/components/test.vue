fd6afb10526220c96a953d6e25b8402d06efc553
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2024-01-28 10:35:05 +0700

    dât for k_group

--- src/components/test.vue
+++ src/components/test.vue
@@ -3,7 +3,9 @@
 " style="height: 60vh;">
     
 <h1 class="text-h1 text-center ">You are in group {{ this.grname }}</h1>
-
+<v-container class="text-center ">
+                <v-btn type="submit" block @click="modifyData" class="mt-2">Submit</v-btn>
+              </v-container>
 </v-container>
 </template>
 
@@ -13,12 +15,13 @@ import { calulatedGroup  } from "../services/store.js";
 export default {
     data() {
     return {
-      k_group: calulatedGroup.data,
       grname: "No Data"
     }
   },
   computed: {
-    
+    k_group() {
+      return calulatedGroup.data;
+    }
   },
   methods: {
     /**
@@ -26,18 +29,22 @@ export default {
      * @returns {string} The group name.
      */
      group2name() {
-      if (this.k_group == 1) {
+      if (this.k_group === 1) {
         this.grname = "good starter";
-      } else if (this.k_group == 2) {
+      } else if (this.k_group === 2) {
         this.grname =  "smart saver";
-      } else if (this.k_group== 3) {
+      } else if (this.k_group=== 3) {
         this.grname = "good saver";
       }
       else{
         this.grname = "zero that not infer the group for you due to lack of data or something."
       }
-    }
+    },
+    modifyData(){
+    console.log(this.k_group)
   }
+  }
+  
 // Component logic goes here
 }
 </script>
